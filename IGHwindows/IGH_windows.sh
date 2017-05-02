ded363e844945a3b054d1b72e0ad2600459b0642
lcolladotor <lcollado@jhsph.edu>
2017-05-01 20:47:33 -0400

    Add RSE objects for the windows

--- IGHwindows/IGH_windows.sh
+++ IGHwindows/IGH_windows.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #$ -cwd
-#$ -N IGH_rse_windows
-#$ -o ./IGH_rse_windows.txt
-#$ -e ./IGH_rse_windows.txt
+#$ -N IGH_windows
+#$ -o ./IGH_windows.txt
+#$ -e ./IGH_windows.txt
 #$ -m e
 
 ## Usage:
-# qsub IGH_rse_windows.sh 
+# qsub IGH_windows.sh 
 
 bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/lieber/ajaffe/lab/insp/IGHwindows/IGHwindows.Rdata" -s "sumsIGHwindows" -c 1
