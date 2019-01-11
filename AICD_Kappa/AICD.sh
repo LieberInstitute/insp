2cd19bd10e41e0a4d3dc140afe581934794f68d6
lcolladotor <lcollado@jhsph.edu>
2019-01-11 17:06:31 -0500

    Fix the input Rdata file

--- AICD_Kappa/AICD.sh
+++ AICD_Kappa/AICD.sh
@@ -8,4 +8,4 @@
 ## Usage:
 # qsub AICD.sh 
 
-bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/ajaffe/data/lab/insp/IGHwindows/IGHwindows.Rdata" -s "sumsAICD" -c 1
+bash /users/lcollado/R/x86_64-pc-linux-gnu-library/3.3.x/recount.bwtool/extdata/jhpce/run_rse.sh -r "/dcl01/ajaffe/data/lab/insp/AICD_Kappa/AICD.Rdata" -s "sumsAICD" -c 1
